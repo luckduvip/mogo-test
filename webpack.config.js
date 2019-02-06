af23e2cd5d900a624be09fd67da44118609973a6
luckduvip <luckduvip@163.com>
2019-02-06 23:10:29 +0800

    ff

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	plugins: [
 		new VueLoaderPlugin()
 	],
-	entry: './src/main.js',
+	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, './dist'),
 		publicPath: '/dist/',
